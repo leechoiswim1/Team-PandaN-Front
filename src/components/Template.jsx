c0c6be211f4906e0595efe787c1701de5a361295
Hyobee <choeple9168@gmail.com>
2021-07-27 23:09:25 +0900

    [레이아웃] 기본 레이아웃 #3 [ETC] bootstrap, bootstrap variables, font, custom style

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
+import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
+import '../styles/fonts/notosanskr/notosanskr.css';
+import '../styles/scss/pandan.scss';
+ 
 const Template = ({ history }) => {
 	return (
 		<React.Fragment>
       <Header />
       <Sidebar />
+			<Button variant="danger">버튼</Button>
 		</React.Fragment>
 	)
 };
