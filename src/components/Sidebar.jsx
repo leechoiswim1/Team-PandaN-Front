9e28d0d93f376c325129da749bddd5d4a60c3169
Hyobee <choeple9168@gmail.com>
2021-08-01 01:39:12 +0900

    [컴포넌트] 사이드바 사이드바 레이아웃 작성 및 아이콘 svg 컴포넌트 작성

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,52 +1,72 @@
 import React from "react";
+
+/* == Library */
 import { Link } from "react-router-dom";
+import { Container, Nav } from "react-bootstrap";
+
+/* == Custom - Component */
 import ProjectModal from "./ProjectModal";
 import Modals from "./Modals";
-import { Container, Nav } from "react-bootstrap";
-import Logo from "../styles/images/logo.svg";
-import IconBookMark from "../styles/images/ico-bookmark.svg";
-import IconDocument from "../styles/images/ico-document.svg";
+
+/* == Custom - Icon */
+import { ReactComponent as Logo } from "../styles/images/logo.svg";
+import { ReactComponent as IconBookMark } from "../styles/images/ico-bookmark.svg";
+import { ReactComponent as IconDocument } from "../styles/images/ico-document.svg";
+import { ReactComponent as IconProject } from "../styles/images/ico-project.svg";
+import { ReactComponent as IconProjectAdd } from "../styles/images/ico-project-add.svg";
+
+// * == (Sidebar) -------------------- * //
 
 const Sidebar = ({ history }) => {
   return (
     <nav className="sidebar" id="sidebar">
-      <Container fluid>
         {/* == 로고 */}
-        <Link to="{/}"><img src={Logo} /></Link>
+        <div className="logo-group">
+          <Container fluid>
+            <Link to="/" className="sidebar-logo"><Logo/></Link>
+          </Container>
+        </div>
 
         {/* == 메뉴 */}
-        <ul className="menu">
-          <li><Link to=""><img src={IconBookMark} />북마크</Link></li>
-          <li><Link to=""><img src={IconDocument} />내가 작성한 문서</Link></li>
-          <li><ProjectModal /></li>
-          <li><Link to=""><img src={IconDocument} />프로젝트</Link></li>
-        </ul>
+        <div className="menu-group">
+          <Container fluid>
+            <ul className="menu">
+              <li className="menu-item active">
+                <Link to="" className="menu-link">
+                  <IconBookMark width="40" height="40" fill="#9A9A9A" className="menu-icon"/>북마크
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="" className="menu-link">
+                  <IconDocument width="40" height="40" fill="#9A9A9A" className="menu-icon"/>내가 작성한 문서
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="" className="menu-link">
+                  <IconProjectAdd width="40" height="40" fill="#9A9A9A" className="menu-icon"/>프로젝트 만들기
+                </Link>
+              </li>
+              <li className="menu-item">
+                <Link to="" className="menu-link">
+                  <IconProject width="40" height="40" fill="#9A9A9A" className="menu-icon"/>프로젝트
+                </Link>
+              </li>            
+            </ul>
+          </Container>
+        </div>
 
         {/* == 푸터 */}
         <div className="footer" id="footer">
-          <Modals
-            buttonTitle="프로젝트 초대코드 등록"
-            Title="초대코드를 등록해주세요! "
-            Content={<input placeholder="어쩌고 저쩌고" />}
-            Summit="등록"
-          />
-          <Modals
-            buttonTitle="버튼제목"
-            Title="타이틀"
-            Content="컨텐츠"
-            Close="Close"
-            Summit="등록"
-          />
-          <Modals
-            buttonTitle="버튼제목"
-            Title="타이틀"
-            Content="컨텐츠"
-            Close="Close"
-            Summit="등록"
-          />
-          <p>Copyright 2021 pandaN</p>
+          <Container fluid>
+            <Modals
+              buttonTitle="프로젝트 초대코드 등록"
+              Title="초대코드를 등록해주세요! "
+              Content={<input placeholder="어쩌고 저쩌고"/>}
+              Summit="등록"
+            />
+            <p className="copyright">Copyright 2021 pandaN</p>
+          </Container>
         </div>
-      </Container>
     </nav>
   );
 };
