8a53191eff309a3610fc25af341d82de42d08050
Hyobee <choeple9168@gmail.com>
2021-08-01 01:38:33 +0900

    [ADD] [컴포넌트] 헤더 헤더 레이아웃 작성 #4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,62 @@
 import React from "react";
-/* styled-components 및 rem 변환 모듈 */
+
+/* == Library */
 import styled, { css } from "styled-components";
-import { AlignRight } from "react-feather";
 import { t } from "../util/remConverter";
-import { Container, Button } from "react-bootstrap";
+import { Container, Col, Row, InputGroup, 
+Form, FormControl, Button, Dropdown, DropdownButton } from "react-bootstrap";
+
+/* == Library - Icon (react-feather) */
+// https://feathericons.com/?query=logout
+import { AlignRight } from "react-feather";
+import { LogOut } from "react-feather";
+
+/* == Custom - Component */
 import { Modals, EditorModal } from "./";
+
+/* == Custom - Icon */
+import { ReactComponent as IconSearch } from "../styles/images/ico-search.svg";
+import { ReactComponent as IconMemberAdd } from "../styles/images/ico-member-add.svg";
+import { ReactComponent as IconProfile } from "../styles/images/ico-profile.svg";
+
+// * == (Header) -------------------- * //
 
 const Header = ({ history }) => {
   return (
     <header className="header" id="header">
       <Container fluid>
-        <div>
-          <h3>ProjectTitle</h3>
-          <h6>여기에 프로젝트 설명을씁니다 하하하하</h6>
-        </div>
-        <div>
-          <input placeholder="검색" />
-          <EditorModal />
-          <Button>프로필</Button>
-          <Button>문서작성</Button>
-          <Modals
-            buttonTitle="멤버초대"
-            Title="멤버초대"
-            Content="코드받기로넣어줄코드~"
-            Close="Close"
-          />
-          <AlignRight />
-        </div>
+        <Row>
+          <Col lg={2}>
+            {/* == 햄버거 메뉴 */}
+            <Button><AlignRight /></Button>
+          </Col>
+          <Col lg={10} className="d-inline-flex justify-content-end">
+            {/* == 검색창 */}
+            <div className="search-group">
+              <InputGroup className="mb-3">
+                <select className="form-control">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+                <FormControl placeholder="검색어를 입력하세요" />
+                {/* <Button variant="parimary"><img src={IconSearch} /></Button> */}
+                <button>
+                  <IconSearch width="40" height="40" fill="#767676" />
+                </button>
+              </InputGroup>
+            </div>
+
+            {/* == 멤버 추가 */}
+            <button><IconMemberAdd width="40" height="40" fill="#767676" /></button>
+
+            {/* == 유저프로필 */}
+            <button><IconProfile width="40" height="40" fill="#767676" /></button>
+
+            {/* == 로그아웃 */}
+            <button><LogOut /></button>
+          </Col>
+        </Row>
       </Container>
     </header>
   );
