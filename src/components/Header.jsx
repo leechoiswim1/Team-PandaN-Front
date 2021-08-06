08dd717d4f02433290b4a67f5e334be4df2ec426
Hyobee <choeple9168@gmail.com>
2021-08-06 19:43:02 +0900

    [VIEW] [컴포넌트] 헤더 header 작성 #4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,8 @@ import { Container, Col, Row, InputGroup,
 Form, FormControl, Button, Dropdown, DropdownButton } from "react-bootstrap";
 
 /* == Library - Icon (react-feather) */
-// https://feathericons.com/?query=logout
+// https://feathericons.com/
 import { AlignRight } from "react-feather";
-import { LogOut } from "react-feather";
 
 /* == Custom - Component */
 import { Modals, EditorModal } from "./";
@@ -26,35 +25,49 @@ const Header = ({ history }) => {
     <header className="header" id="header">
       <Container fluid>
         <Row>
-          <Col lg={2}>
+          <Col>
             {/* == 햄버거 메뉴 */}
-            <Button><AlignRight /></Button>
+            <Button id="btn-hamburger"><AlignRight/></Button>
           </Col>
-          <Col lg={10} className="d-inline-flex justify-content-end">
+          <Col className="d-inline-flex justify-content-end">
             {/* == 검색창 */}
-            <div className="search-group">
+            {/* <div className="search-group">
               <InputGroup className="mb-3">
                 <select className="form-control">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
+                  <option>전체</option>
+                  <option>게시글 제목</option>
+                  <option>게시글 상태</option>
                 </select>
-                <FormControl placeholder="검색어를 입력하세요" />
-                {/* <Button variant="parimary"><img src={IconSearch} /></Button> */}
+                <FormControl placeholder="검색어를 입력하세요"/>
                 <button>
-                  <IconSearch width="40" height="40" fill="#767676" />
+                  <IconSearch width="40" height="40" fill="#767676"/>
                 </button>
               </InputGroup>
-            </div>
+            </div> */}
 
             {/* == 멤버 추가 */}
-            <button><IconMemberAdd width="40" height="40" fill="#767676" /></button>
+            {/* <button><IconMemberAdd width="40" height="40" fill="#767676"/></button> */}
 
             {/* == 유저프로필 */}
-            <button><IconProfile width="40" height="40" fill="#767676" /></button>
+            {/* <button><IconProfile width="40" height="40" fill="#767676" /></button> */}
+            <Dropdown>
+              <Dropdown.Toggle variant="success" align="end">
+                <IconProfile width="35" height="35" fill="#ffffff" />
+              </Dropdown.Toggle>
 
-            {/* == 로그아웃 */}
-            <button><LogOut /></button>
+              <Dropdown.Menu className="dropdown-group">
+                <Dropdown.ItemText className="text-center">
+                  <IconProfile width="40" height="40" fill="#ffffff" className="dropdown-profile"/>
+                  <p className="dropdown-name">User Name</p>
+                  <p className="dropdown-email">UserId_123456789@gmail.com</p>
+                </Dropdown.ItemText>
+                <Dropdown.Divider style={{height: "0"}}/>
+                <Dropdown.ItemText>
+                  {/* == 로그아웃 */}
+                  <Button variant="primary" size="sm" className="d-block w-100">로그아웃</Button>
+                </Dropdown.ItemText>
+              </Dropdown.Menu>
+            </Dropdown>
           </Col>
         </Row>
       </Container>
