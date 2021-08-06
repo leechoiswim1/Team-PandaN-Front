9010d4625b5b451a8d2271945e905f04fadd7d42
Hyobee <choeple9168@gmail.com>
2021-08-06 19:45:42 +0900

    [VIEW] [컴포넌트] 사이드바 사이드바 작성  #5

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
 /* == Library */
 import { Link } from "react-router-dom";
-import { Container, Nav } from "react-bootstrap";
+import { Container, Button, Collapse, Accordion, Card } from "react-bootstrap";
+
+/* == Library - Icon (react-feather) */
+// https://feathericons.com/
+import { ChevronDown } from "react-feather";
+import { X } from "react-feather";
 
 /* == Custom - Component */
 import ProjectModal from "./ProjectModal";
@@ -10,6 +16,7 @@ import Modals from "./Modals";
 
 /* == Custom - Icon */
 import { ReactComponent as Logo } from "../styles/images/logo.svg";
+import LogoImg from "../styles/images/favicon.png";
 import { ReactComponent as IconBookMark } from "../styles/images/ico-bookmark.svg";
 import { ReactComponent as IconDocument } from "../styles/images/ico-document.svg";
 import { ReactComponent as IconProject } from "../styles/images/ico-project.svg";
@@ -19,11 +26,15 @@ import { ReactComponent as IconProjectAdd } from "../styles/images/ico-project-a
 
 const Sidebar = ({ history }) => {
   return (
-    <nav className="sidebar" id="sidebar">
+    <nav className="sidebar">
         {/* == 로고 */}
         <div className="logo-group">
           <Container fluid>
-            <Link to="/" className="sidebar-logo"><Logo/></Link>
+            <Link to="/" className="sidebar-logo">
+              <Logo className="logo"/>
+              <img src={LogoImg} alt="" className="logo-mobile"/>
+              <button id="btn-close-sidebar" className="btn-close-sidebar"><X/></button>
+            </Link>
           </Container>
         </div>
 
@@ -33,23 +44,40 @@ const Sidebar = ({ history }) => {
             <ul className="menu">
               <li className="menu-item active">
                 <Link to="" className="menu-link">
-                  <IconBookMark width="40" height="40" fill="#9A9A9A" className="menu-icon"/>북마크
+                  <IconBookMark width="40" height="40" fill="#9A9A9A" className="menu-icon"/> 
+                  <span className="menu-text">북마크</span>
                 </Link>
               </li>
               <li className="menu-item">
                 <Link to="" className="menu-link">
-                  <IconDocument width="40" height="40" fill="#9A9A9A" className="menu-icon"/>내가 작성한 문서
+                  <IconDocument width="40" height="40" fill="#9A9A9A" className="menu-icon"/>
+                  <span className="menu-text">내가 작성한 문서</span>
                 </Link>
               </li>
               <li className="menu-item">
                 <Link to="" className="menu-link">
-                  <IconProjectAdd width="40" height="40" fill="#9A9A9A" className="menu-icon"/>프로젝트 만들기
+                  <IconProjectAdd width="40" height="40" fill="#9A9A9A" className="menu-icon"/>
+                  <span className="menu-text">프로젝트 만들기</span>
                 </Link>
               </li>
               <li className="menu-item">
-                <Link to="" className="menu-link">
-                  <IconProject width="40" height="40" fill="#9A9A9A" className="menu-icon"/>프로젝트
-                </Link>
+                <Accordion defaultActiveKey="0" className="my-project-group">
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                      <IconProject width="40" height="40" fill="#9A9A9A" className="menu-icon"/>
+                      <span className="menu-text">내 프로젝트 보기</span></Accordion.Header>
+                    <Accordion.Body>
+                      <ul className="project-list">
+                        <li className="project-item active"><Link to="" className="project-link">프로젝트 1</Link></li>
+                        <li className="project-item"><Link to="" className="project-link">프로젝트 2</Link></li>
+                        <li className="project-item"><Link to="" className="project-link">프로젝트 3</Link></li>
+                        <li className="project-item"><Link to="" className="project-link">프로젝트 4</Link></li>
+                        <li className="project-item"><Link to="" className="project-link">프로젝트 5</Link></li>
+                        <li className="project-item"><Button variant="primary" size="sm" className="d-block w-100 mt-10"><span className="menu-text">내 프로젝트 더보기</span></Button></li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
               </li>            
             </ul>
           </Container>
@@ -58,12 +86,15 @@ const Sidebar = ({ history }) => {
         {/* == 푸터 */}
         <div className="footer" id="footer">
           <Container fluid>
-            <Modals
+            <div className="d-grid gap-2">
+              <Button variant="primary" size="lg" className="d-block">프로젝트 초대코드 등록</Button>
+            </div>
+            {/* <Modals
               buttonTitle="프로젝트 초대코드 등록"
               Title="초대코드를 등록해주세요! "
               Content={<input placeholder="어쩌고 저쩌고"/>}
               Summit="등록"
-            />
+            /> */}
             <p className="copyright">Copyright 2021 pandaN</p>
           </Container>
         </div>
