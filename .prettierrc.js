a7e9d9b58f32388c536be2abb55a7f4c4b4d2ba5
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-08 23:27:17 -0400

    [FORMAT] .prettier 설정 일부 변경
    - 기존의 옵션 잘못 설정한 부분 수정합니다.

--- .prettierrc.js
+++ .prettierrc.js
@@ -11,8 +11,10 @@ module.exports = {
   tabWidth: 2,
   // 객체나 배열을 작성 할 때, 원소 혹은 key-value의 맨 뒤에 있는 것에도 쉼표를 붙임
   trailingComma: "all",
-  // 코드 한줄이 maximum 80칸
-  printWidth: 80,
-  // 화살표 함수가 하나의 매개변수를 받을 때 괄호 생략
-  arrowParens: "avoid",
+  // 코드 한줄이 maximum 150칸
+  printWidth: 150,
+  // 화살표 함수가 하나의 매개변수를 받을 때 괄호 표기
+  arrowParens: "always",
+  // jsx > 한 줄 내려 표기
+  jsxBracketSameLine: false,
 };
