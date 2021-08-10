04ffd724826cee97749aadde271a59d25dfb78c9
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-10 01:23:54 -0400

    [UPDATE] 프로젝트 전체 노트 / 내가 작성한 노트 테스트 완료 #39
    - 응답 속성 변경에 따라 일부 수정 및 작성일 출력되도록 변경

--- src/components/notes/IssueCard.jsx
+++ src/components/notes/IssueCard.jsx
@@ -2,7 +2,14 @@ import React, { useEffect } from "react";
 /* == Library - style */
 import styled from "styled-components";
 import { t }  from "../../util/remConverter";
-import { CheckSquare, Bookmark } from "react-feather";
+import { Bookmark } from "react-feather";
+
+/* == Library - date */
+import moment from "moment";
+
+/* == Custom - Icon */
+import IconSteps                        from "../../elements/IconSteps";
+
 /* == Redux - actions */
 import { useDispatch }   from 'react-redux';
 import { noteActions }                from '../../modules/note';
@@ -10,7 +17,8 @@ import { noteActions }                from '../../modules/note';
 // * == ( IssueCard / Note ) -------------------- * //
 const IssueCard = (props) => {
   const dispatch = useDispatch();
-  const {projectId, noteId, title, content, deadline, step, ...rest} = props; 
+  const { projectId, noteId, title, content, deadline, step, createdAt, ...rest } = props; 
+  const created = moment(createdAt).format("작성일: YYYY년 M월 D일");
 
   const deleteBookmark = (e) => {
     e.preventDefault();
@@ -25,17 +33,14 @@ const IssueCard = (props) => {
     <Wrapper>
       <div style={{display: "flex", flexDirection: "row"}}>
         <div>
-          <CheckSquare />
-          {step}
+          <IconSteps type={step}/> 
         </div>
         <NoteDesc>
           <a href={`/projects/${projectId}/notes/${noteId}`}>{title}</a>
           <div>
-            { rest.projectTitle ? 
-              <span className="text-primary">프로젝트 이름</span> :
-              <span className="text-primary">작성자</span>
-            }         
-            <span>{deadline}</span>
+            { rest.projectTitle && <span className="text-primary">프로젝트 이름</span> }
+            { rest.author && <span className="text-primary">작성자</span> }
+            { createdAt && <span>{created}</span> }
           </div>          
         </NoteDesc>
       </div>
@@ -64,8 +69,8 @@ const NoteDesc = styled.div(...t`
     font-weight: 500;
     text-decoration: none;
   }
-  div span:last-child {
-    margin-left: 16px;
+  div :last-child {
+    margin-rifgt: 16px;
     color: #888;
   }
 `)
