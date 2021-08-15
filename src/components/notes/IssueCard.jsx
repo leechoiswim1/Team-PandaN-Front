913cb2ec8c0af4e58ffae3f47b79b1ee6b4696d2
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-15 16:07:32 -0400

    [VIEW] 게시판형 페이지 > 노트 스텝 레이블 추가 #57
    - 노트 스텝 레이블 추가

--- src/components/notes/IssueCard.jsx
+++ src/components/notes/IssueCard.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect } from "react";
-/* == Library - style */
-import styled, { css } from "styled-components";
-import { t }  from "../../util/remConverter";
-import { Bookmark } from "react-feather";
-
+import React              from "react";
+/* == Library - icon */
+import { Bookmark }       from "react-feather";
 /* == Library - date */
-import moment from "moment";
-
+import moment             from "moment";
 /* == Library - router */
-import { Link } from "react-router-dom";
+import { Link }           from "react-router-dom";
 
-/* == Custom - Icon */
-import IconSteps                        from "../../elements/IconSteps";
+/* == Custom - icon */
+import IconSteps          from "../../elements/IconSteps";
+/* == Custom - elements */
+import Labels             from "../../elements/Labels";
 
 /* == Redux - actions */
-import { useDispatch }   from 'react-redux';
-import { noteActions }                from '../../modules/note';
+import { useDispatch }    from 'react-redux';
+import { noteActions }    from '../../modules/note';
 
 // * == ( IssueCard / Note ) -------------------- * //
 const IssueCard = (props) => {
@@ -53,10 +51,7 @@ const IssueCard = (props) => {
           <Link to={`/projects/${projectId}/notes/${noteId}`}>
             <h1>{title}</h1>
           </Link>
-          <div>
-            {/* <Tag className="kanban-card-tag" type={step}>
-              { step }
-            </Tag> */}
+          <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
             { projectTitle && 
               <Link to={`/projects/${projectId}/kanban`}>
                 <span className="note-issuecard-title">{projectTitle}</span>
@@ -64,6 +59,9 @@ const IssueCard = (props) => {
             }
             { writer && <span>{writer}</span> }
             { createdAt && <span>{created}</span> }
+            <Labels type={step} >
+              {step}
+            </Labels>
           </div>          
         </div>
       </div>
@@ -77,24 +75,5 @@ const IssueCard = (props) => {
     </div>
   );
 };
-
-// const Tag = styled.div`
-// ${(props) => (props.type === "STORAGE") && 
-//   css`  
-//     background-color: #FFCD40;
-//   `}
-// ${(props) => (props.type === "TODO") && 
-//   css`  
-//     background-color: #ADBE4F;
-//   `}
-// ${(props) => (props.type === "PROCESSING") && 
-// css`  
-//   background-color: #9BD09C;
-// `}
-// ${(props) => (props.type === "DONE") && 
-//   css`  
-//     background-color: #F5DAAE;
-//   `}
-// `
 
 export default IssueCard;
