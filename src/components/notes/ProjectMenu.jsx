2b63e4f36857b514abf5b43a73683f31fa46d148
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-15 05:55:17 -0400

    [ADD] 프로젝트 메뉴 내 노트 작성버튼 추가 #56
    - 프로젝트 메뉴 내 노트 작성버튼 추가
    - 프로젝트 진입 후 전체문서 / 내가 작성한 문서 페이지에서도 노트 작성할 수 있도록 개선
    - 위치는 협의 후 변경 가능

--- src/components/notes/ProjectMenu.jsx
+++ src/components/notes/ProjectMenu.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState }   from "react";
+
 /* == Library */
 import { NavLink } from "react-router-dom";
 
+/* == Library - bootstrap */
+import { Button } from "react-bootstrap";
+
+/* == Custom - Component */
+import { WritingNoteModal } from "..";
+
+/* == Custom - Icon */
+import { ReactComponent as Write }      from "../../styles/images/ico-kanban-write.svg";
+
 // * == ( Note / project menu ) -------------------- * //
 const ProjectMenu = ({ history, match, projectId, ...rest }) => {
+  /* 
+  * Function - Modal
+  * menuModalVisible : kanban > 노트 작성 모달의 state와 구분
+  */
+  const [menuModalVisible, setMenuModalVisible] = useState(false)
+  const openModal  = () => {setMenuModalVisible(true)}
+  const closeModal = () => {setMenuModalVisible(false)}
+
   return (
     <div className="project-menu"> 
+      {/* project menu */}
       <nav>
         <ul>
           <li>
@@ -25,6 +44,20 @@ const ProjectMenu = ({ history, match, projectId, ...rest }) => {
           </li>
         </ul>
       </nav>
+
+      {/* writing note modal */}
+      <div>
+        <Button variant="primary" size="sm" onClick={openModal}>
+          <span className="menu-text">
+            <Write fill="#FFFFFF" width="14" height="14" style={{margin: "-2px 8px 0 0"}} />
+            할 일 만들기
+          </span>
+        </Button>
+        { menuModalVisible && 
+          <WritingNoteModal 
+            projectId={projectId} visible={menuModalVisible} closable={true} maskClosable={true} onClose={closeModal} />
+        }      
+      </div>
     </div>
   );
 };
