7d017fb995b77dfd3881547dfcaca98037639048
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-16 14:21:10 -0400

    [FORMAT] 쿠키 설정/삭제 함수 코드 정리 및 주석 추가

--- src/shared/cookie.js
+++ src/shared/cookie.js
@@ -1,9 +1,18 @@
+/**
+ * setCookie
+ * @param {string} name : cookie name
+ * @param {*} value 		: cookie value
+ * @param {*} exp 			: 만료시간 지정
+ */
 const setCookie = (name, value, exp = 1) => {
 	let date = new Date();
 	date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
 	document.cookie = `${name}=${value}; expires=${date.toUTCString()}`;
 };
-
+/**
+ * deleteCookie
+ * @param {string} name : cookie name
+ */
 const deleteCookie = (name) => {
 	let date = new Date("2020-01-01").toUTCString();
 	document.cookie = name + "=; expires=" + date;
