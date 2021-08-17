7ebc55b7c429751995754c987c589107bc4d6b06
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-17 09:24:54 -0400

    [ETC] 로그인 관련 모듈 수정 및 주석 작성

--- src/shared/cookie.js
+++ src/shared/cookie.js
@@ -1,14 +1,15 @@
 /**
  * setCookie
  * @param {string} name : cookie name
- * @param {*} value 		: cookie value
- * @param {*} exp 			: 만료시간 지정(일 단위, 1 = 1일, 현재 기본값 1일이며 배포 시 수정 예정)
+ * @param {*} value     : cookie value
+ * @param {*} exp       : 만료시간 지정(일 단위, 1 = 1일, 현재 기본값 1일이며 배포 시 수정 예정)
  */
 const setCookie = (name, value, exp = 1) => {
 	let date = new Date();
 	date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
 	document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";	
 };
+
 /**
  * deleteCookie
  * @param {string} name : cookie name
