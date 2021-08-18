1e173382cc39ae53f444f4c726c0ddb0c5335b5a
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-18 09:38:34 -0400

    [FORMAT] user 모듈 주석 작성

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,32 +1,82 @@
-import { createAction, handleActions } from "redux-actions";
-/* == Library - jwt decode */
-import jwtDecode from "jwt-decode";
-/* == Custom - shared > api / cookie */
-import { userApi } from "../shared/api";
-import { setCookie, deleteCookie } from "../shared/cookie";
+/**
+ * --------------------------------------------------------------------------
+ * Redux Module: user.js
+  * 기능 (로그인)
+    * [로그인] - 서비스 접근을 위해 OAuth 인가코드를 서버로 보내 사용자 인증을 받으며 
+    *           jwt token을 응답으로 받음
+    * 
+    * [로그아웃] - 로그인 상태에서 서비스 이용을 종료하며, 
+    *            쿠키에 저장된 토큰과 로컬스토리지에 저장된 사용자 정보를 삭제함
+    * 
+    * [로그인 유지] - 새로고침시 사용자의 로그인 여부를 체크하여 
+    *               로그인 한 사용자의 경우 로그인 상태 유지
+ * --------------------------------------------------------------------------
+ */
 
-/* == User - initial state */
+/**
+ * --------------------------------------------------------------------------
+ * import
+    * [Library] redux-actions
+    * [Library] jwt-decode: jwt 토큰 복호화
+    * [Custom] api: axios instance 및 api 요청 함수
+    * [Custom] cookie: 쿠키 저장 및 삭제 함수
+ * --------------------------------------------------------------------------
+ */
+
+import { createAction, handleActions }  from "redux-actions";
+import jwtDecode                        from "jwt-decode";
+import { userApi }                      from "../shared/api";
+import { setCookie, deleteCookie }      from "../shared/cookie";
+
+/**
+ * --------------------------------------------------------------------------
+ * initial state
+ * --------------------------------------------------------------------------
+ */
+
 const initialState = {
   name: "",
   email: "",
-  picture: "https://e7.pngegg.com/pngimages/287/501/png-clipart-giant-panda-emoji-coloring-book-drawing-sticker-emoji-child-face-thumbnail.png",
+  picture: "",
   isLoggedIn: false,
 };
 
-/* == action */
-const LOGIN = "user/LOGIN";
-const LOGOUT = "user/LOGOUT";
+/**
+ * --------------------------------------------------------------------------
+ * action
+ * --------------------------------------------------------------------------
+ */
+
+const LOGIN     = "user/LOGIN";
+const LOGOUT    = "user/LOGOUT";
 const SET_LOGIN = "user/SET_LOGIN";
 
-/* == action creator */
-const login = createAction(LOGIN, (user) => ({ user }));
-const logout = createAction(LOGOUT, () => ({}));
-const setLogin = createAction(SET_LOGIN, (user) => ({ user }));
+/**
+ * --------------------------------------------------------------------------
+ * action creator
+ * --------------------------------------------------------------------------
+ */
 
-/* == thunk function */
+const login     = createAction(LOGIN, (user) => ({ user }));
+const logout    = createAction(LOGOUT, () => ({}));
+const setLogin  = createAction(SET_LOGIN, (user) => ({ user }));
+
+
+/**
+ * --------------------------------------------------------------------------
+ * middleware thunk function
+ * --------------------------------------------------------------------------
+ */
+
+/**
+ * __login()
+    * OAuth 인가코드 서버로 보내고, 응답으로 jwt 받음
+    * 토큰은 cookie에 저장, 복호화 한 후 사용자 정보는 localStorage 저장
+    * 사용자 정보 리듀서로 보낸 후 메인 페이지로 이동
+ * @param {string} authorization_code : 로그인 시 리다이렉트된 페이지에서 받은 인가 코드
+ */
 const __login =
   (authorization_code) =>
-
   async (dispatch, getState, { history }) => {
     try {
       const { data } = await userApi.login(authorization_code);
@@ -64,6 +114,12 @@ const __login =
     }
   };
 
+/**
+ * __logout
+    * localStorage / cookie 에 저장된 정보 및 토큰 삭제
+    * 사용자 로그아웃 처리 리듀서로 보낸 후 로그인 페이지로 이동
+ */
+
 const __logout =
   () =>
   (dispatch, getState, { history }) => {
@@ -77,6 +133,13 @@ const __logout =
     }
   };
 
+/**
+ * __setLogin
+    * localStorage / cookie 에 저장된 정보 및 토큰 있는지 확인
+    * 둘 다 있을 경우 사용자 정보 리듀서로 보내 로그인 유지
+    * router path 있는 모든 페이지 내에서 새로고침 시에도 로그인 정보 store 상에서 유지 목적 
+ */
+
 const __setLogin =
   () =>
   (dispatch, getState, { history }) => {
@@ -88,7 +151,12 @@ const __setLogin =
     }
   };
 
-/* == reducer */
+/**
+ * --------------------------------------------------------------------------
+ * reducer
+ * --------------------------------------------------------------------------
+ */
+
 const user = handleActions(
   {
     [LOGIN]: (state, action) => {
@@ -119,7 +187,12 @@ const user = handleActions(
   initialState,
 );
 
-/* == export actions */
+/**
+ * --------------------------------------------------------------------------
+ * export actions
+ * --------------------------------------------------------------------------
+ */
+
 export const userActions = {
   __login,
   __logout,
