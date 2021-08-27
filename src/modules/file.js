470b65f4c10c4e1b9951aca50de85bfb808950b9
YoungKwak <80361834+young-kwak@users.noreply.github.com>
2021-08-27 03:58:24 -0400

    [ETC] file 모듈 미사용분 주석 처리

--- src/modules/file.js
+++ src/modules/file.js
@@ -33,9 +33,9 @@ const initialState = {
  */
 
 /* file */
-const ADD_FILES       = "file/ADD_FILES";
-const EDIT_FILES      = "file/EDIT_FILES";
-const DELETE_FILE     = "file/DELETE_FILE";
+// const ADD_FILES       = "file/ADD_FILES";
+// const EDIT_FILES      = "file/EDIT_FILES";
+// const DELETE_FILE     = "file/DELETE_FILE";
 /* file preview */
 const SET_PREVIEW     = "file/SET_PREVIEW";
 const RESET_PREVIEW   = "file/RESET_PREVIEW";
@@ -48,9 +48,9 @@ const DELETE_PREVIEW  = "file/DELETE_PREVIEW";
  */
 
 /* file */ 
-const addFiles      = createAction(ADD_FILES , () => ({}));
-const editFiles     = createAction(EDIT_FILES, () => ({}));
-const deleteFile    = createAction(DELETE_FILE, () => ({}));
+// const addFiles      = createAction(ADD_FILES , () => ({}));
+// const editFiles     = createAction(EDIT_FILES, () => ({}));
+// const deleteFile    = createAction(DELETE_FILE, () => ({}));
 /* file preview */
 const setPreview    = createAction(SET_PREVIEW, ( fileName, awsFileName, fileUrl ) => ({ fileName, awsFileName, fileUrl }));
 const resetPreview  = createAction(RESET_PREVIEW, () => ({}));
@@ -62,51 +62,51 @@ const deletePreview = createAction(DELETE_PREVIEW, ( awsFileName ) => ({ awsFile
  * --------------------------------------------------------------------------
  */
  
-const __addFiles =
-  () =>
-  async (dispatch, getState, { history }) => {
-    try {
-      const fileList = getState().file.files;
-      if ( fileList === [] || fileList.length - 1 > 5) return;
-      // awsFileName 제거
-      fileList.map(file => delete file.awsFileName);
-      const newFiles = {
-        files: fileList
-      }
-      console.log("request body", newFiles) 
-      // const { data } = await fileApi.addFiles(newFiles);
+// const __addFiles =
+//   () =>
+//   async (dispatch, getState, { history }) => {
+//     try {
+//       const fileList = getState().file.files;
+//       if ( fileList === [] || fileList.length - 1 > 5) return;
+//       // awsFileName 제거
+//       fileList.map(file => delete file.awsFileName);
+//       const newFiles = {
+//         files: fileList
+//       }
+//       console.log("request body", newFiles) 
+//       // const { data } = await fileApi.addFiles(newFiles);
       
-    } catch (e) {
-      console.log(e);
-    }
-  };
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
  
-const __editFiles =
-  (noteId) =>
-  async (dispatch, getState, { history }) => {
-    try {
-      // const oldFileList = getState().note.detail.files;
-      // const newFileList = getState().file.files;
-      // if ( newFileList === [] || newFileList.length - 1 > 5) return;
-      // // awsFileName 제거
-      // newFileList.map(file => delete file.awsFileName);
+// const __editFiles =
+//   (noteId) =>
+//   async (dispatch, getState, { history }) => {
+//     try {
+//       // const oldFileList = getState().note.detail.files;
+//       // const newFileList = getState().file.files;
+//       // if ( newFileList === [] || newFileList.length - 1 > 5) return;
+//       // // awsFileName 제거
+//       // newFileList.map(file => delete file.awsFileName);
 
-      // const _newFileList = newFileList.filter(newFile => !oldFileList.includes(newFile));
-      // const { data } = await fileApi.editFiles(noteId, _newFileList);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+//       // const _newFileList = newFileList.filter(newFile => !oldFileList.includes(newFile));
+//       // const { data } = await fileApi.editFiles(noteId, _newFileList);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
 
-const __deleteFile =
-  () =>
-  async (dispatch, getState, { history }) => {
-    try {
-      const { data } = await fileApi.deleteFile();
-    } catch (e) {
-      console.log(e);
-    }
-  };
+// const __deleteFile =
+//   () =>
+//   async (dispatch, getState, { history }) => {
+//     try {
+//       const { data } = await fileApi.deleteFile();
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
 
 /**
  * --------------------------------------------------------------------------
@@ -116,21 +116,21 @@ const __deleteFile =
  
 const file = handleActions(
   {
-    [ADD_FILES]: (state, action) => {
-      return {
-        ...state,
-      };
-    },
-    [EDIT_FILES]: (state, action) => {
-      return {
-        ...state,
-      };
-    },
-    [DELETE_FILE]: (state, action) => {
-      return {
-        ...state,
-      };  
-    },
+    // [ADD_FILES]: (state, action) => {
+    //   return {
+    //     ...state,
+    //   };
+    // },
+    // [EDIT_FILES]: (state, action) => {
+    //   return {
+    //     ...state,
+    //   };
+    // },
+    // [DELETE_FILE]: (state, action) => {
+    //   return {
+    //     ...state,
+    //   };  
+    // },
     [SET_PREVIEW]: (state, action) => {
       return {
         ...state,
@@ -167,9 +167,9 @@ const file = handleActions(
  */
  
 export const fileActions = {
-  __addFiles,
-  __editFiles,
-  __deleteFile,
+  // __addFiles,
+  // __editFiles,
+  // __deleteFile,
   setPreview,
   resetPreview,
   deletePreview,
